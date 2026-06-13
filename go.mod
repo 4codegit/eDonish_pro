d3f81ba8edfa166fb8cec0785f52f10513873dbd
Narziev <narziev@example.com>
2026-06-13 23:45:47 +0500

    feat: Full app with GUI option

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/4codegit/eDonish_pro
 
 go 1.21
+
+require fyne.io/fyne/v2 v2.4.0
---
+++ main_gui.go
@@ -0,0 +1,36 @@
+// +build ignore
+
+package main
+
+import (
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("eDonish Pro - GUI")
+
+	username := widget.NewEntry()
+	username.SetPlaceHolder("Логин")
+
+	password := widget.NewPasswordEntry()
+	password.SetPlaceHolder("Пароль")
+
+	loginBtn := widget.NewButton("Войти", func() {
+		// TODO: Login logic
+		widget.NewLabel("Login clicked!")
+	})
+
+	content := container.NewVBox(
+		widget.NewLabel("eDonish Pro"),
+		username,
+		password,
+		loginBtn,
+	)
+
+	w.SetContent(content)
+	w.Resize(fyne.NewSize(400, 300))
+	w.ShowAndRun()
+}
