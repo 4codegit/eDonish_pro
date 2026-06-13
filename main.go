694e3cc767ccbb9e27e7b1875cb66fa54d9846c9
Narziev <narziev@example.com>
2026-06-13 16:26:53 +0500

    fix: Single file app - no dependencies

--- main.go
+++ main.go
@@ -3,12 +3,29 @@ package main
 import (
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("eDonish Pro")
-	w.SetContent(NewLoginScreen())
+	
+	username := widget.NewEntry()
+	username.SetPlaceHolder("Логин")
+	
+	password := widget.NewPasswordEntry()
+	password.SetPlaceHolder("Пароль")
+	
+	loginBtn := widget.NewButton("Войти", func() {})
+	
+	content := fyne.NewContainerWithLayout(fyne.NewVBox(
+		widget.NewLabel("eDonish Pro"),
+		username,
+		password,
+		loginBtn,
+	))
+	
+	w.SetContent(content)
 	w.Resize(fyne.NewSize(400, 300))
 	w.ShowAndRun()
 }
