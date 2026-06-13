884524c3cfa57852018c0bbbe9a333120066aad1
Narziev <narziev@example.com>
2026-06-14 00:41:36 +0500

    test: http+json+io

--- main.go
+++ main.go
@@ -3,15 +3,23 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 )
-
-type Test struct {
-	Name string `json:"name"`
-}
 
 func main() {
 	fmt.Println("eDonish Pro v0.2.0")
-	t := Test{Name: "test"}
-	json.Marshal(t)
-	fmt.Println("JSON works!")
+	
+	resp, err := http.Get("https://edonish.tj")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer resp.Body.Close()
+	
+	body, _ := io.ReadAll(resp.Body)
+	var data map[string]interface{}
+	json.Unmarshal(body, &data)
+	
+	fmt.Println("HTTP + JSON + IO works!")
 }
