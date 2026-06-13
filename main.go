bf48d105882be8d3936787c37c3593804e8bc2e5
Narziev <narziev@example.com>
2026-06-14 00:52:52 +0500

    feat: Final version

--- main.go
+++ main.go
@@ -1,13 +1,34 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 )
 
 func main() {
 	fmt.Println("eDonish Pro v0.2.0")
+	
 	if len(os.Args) >= 2 {
-		fmt.Println("Args:", os.Args[1])
+		username := os.Args[1]
+		fmt.Println("User:", username)
+		
+		reqBody, _ := json.Marshal(map[string]string{"username": username})
+		resp, err := http.Post("https://edonish.tj/auth/v1/login", "application/json", nil)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		defer resp.Body.Close()
+		
+		body, _ := io.ReadAll(resp.Body)
+		var result map[string]interface{}
+		json.Unmarshal(body, &result)
+		
+		fmt.Println("Response:", result)
+	} else {
+		fmt.Println("Usage: edonish-pro <username>")
 	}
 }
