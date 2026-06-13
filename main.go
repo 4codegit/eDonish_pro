31703a1ac4bcf051313d0f4152299bef955732a9
Narziev <narziev@example.com>
2026-06-14 01:02:26 +0500

    test: reordered imports

--- main.go
+++ main.go
@@ -1,34 +1,35 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"os"
+	
+	"encoding/json"
 	"io"
 	"net/http"
-	"os"
 )
 
 func main() {
 	fmt.Println("eDonish Pro v0.2.0")
 	
-	if len(os.Args) >= 2 {
-		username := os.Args[1]
-		fmt.Println("User:", username)
-		
-		reqBody, _ := json.Marshal(map[string]string{"username": username})
-		resp, err := http.Post("https://edonish.tj/auth/v1/login", "application/json", nil)
-		if err != nil {
-			fmt.Println("Error:", err)
-			return
-		}
-		defer resp.Body.Close()
-		
-		body, _ := io.ReadAll(resp.Body)
-		var result map[string]interface{}
-		json.Unmarshal(body, &result)
-		
-		fmt.Println("Response:", result)
-	} else {
+	if len(os.Args) < 2 {
 		fmt.Println("Usage: edonish-pro <username>")
+		return
 	}
+	
+	username := os.Args[1]
+	fmt.Println("User:", username)
+	
+	data := map[string]string{"username": username}
+	body, _ := json.Marshal(data)
+	
+	resp, err := http.Post("https://edonish.tj/auth/v1/login", "application/json", nil)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer resp.Body.Close()
+	
+	result, _ := io.ReadAll(resp.Body)
+	fmt.Println("Response:", string(result))
 }
