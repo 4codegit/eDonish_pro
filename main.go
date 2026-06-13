0edf3bd70d538c80634cf3807ebc400e94b9f019
Narziev <narziev@example.com>
2026-06-13 17:54:36 +0500

    fix: Full Fyne workflow with all deps

--- main.go
+++ main.go
@@ -2,21 +2,13 @@ package main
 
 import (
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("eDonish Pro")
-
-	content := container.NewVBox(
-		widget.NewLabel("eDonish Pro"),
-		widget.NewButton("Click", func() {}),
-	)
-
-	w.SetContent(content)
-	w.Resize(fyne.NewSize(400, 300))
+	w.SetContent(widget.NewLabel("Hello eDonish Pro!"))
 	w.ShowAndRun()
 }
 
