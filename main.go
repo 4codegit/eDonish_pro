010d6c7ddb28b880277f95bbacd5ec9dd799cdee
Narziev <narziev@example.com>
2026-06-14 01:07:10 +0500

    test: fresh file

--- main.go
+++ main.go
@@ -3,33 +3,11 @@ package main
 import (
 	"fmt"
 	"os"
-	
-	"encoding/json"
-	"io"
-	"net/http"
 )
 
 func main() {
-	fmt.Println("eDonish Pro v0.2.0")
-	
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: edonish-pro <username>")
-		return
+	fmt.Println("eDonish Pro")
+	if len(os.Args) > 1 {
+		fmt.Println("Arg:", os.Args[1])
 	}
-	
-	username := os.Args[1]
-	fmt.Println("User:", username)
-	
-	data := map[string]string{"username": username}
-	body, _ := json.Marshal(data)
-	
-	resp, err := http.Post("https://edonish.tj/auth/v1/login", "application/json", nil)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-	defer resp.Body.Close()
-	
-	result, _ := io.ReadAll(resp.Body)
-	fmt.Println("Response:", string(result))
 }
