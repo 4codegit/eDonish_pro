3e004384c56073d7c90ea2d27a8885e81fe064ff
Narziev <narziev@example.com>
2026-06-13 17:05:50 +0500

    test: Simple Go app without Fyne

--- main.go
+++ main.go
@@ -1,32 +1,8 @@
 package main
 
-import (
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/widget"
-)
+import "fmt"
 
 func main() {
-	a := app.New()
-	w := a.NewWindow("eDonish Pro")
-	
-	username := widget.NewEntry()
-	username.SetPlaceHolder("Логин")
-	
-	password := widget.NewPasswordEntry()
-	password.SetPlaceHolder("Пароль")
-	
-	loginBtn := widget.NewButton("Войти", func() {})
-	
-	content := fyne.NewContainerWithLayout(fyne.NewVBox(
-		widget.NewLabel("eDonish Pro"),
-		username,
-		password,
-		loginBtn,
-	))
-	
-	w.SetContent(content)
-	w.Resize(fyne.NewSize(400, 300))
-	w.ShowAndRun()
+	fmt.Println("eDonish Pro")
 }
 
